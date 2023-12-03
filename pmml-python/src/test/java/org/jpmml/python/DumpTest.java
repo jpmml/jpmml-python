116161da061ebb15aace7646112bd63be86f7529
Villu Ruusmann <villu.ruusmann@gmail.com>
2023-12-03 10:34:47 +0200

    Ensured compatibility with Pandas 2.1(.3)

--- pmml-python/src/test/java/org/jpmml/python/DumpTest.java
+++ pmml-python/src/test/java/org/jpmml/python/DumpTest.java
@@ -164,18 +164,21 @@ public class DumpTest extends PickleUtilTest {
 		unpicklePandasSeries("python-3.9_pandas-1.5.1");
 		unpicklePandasSeries("python-3.9_pandas-1.5.2");
 		unpicklePandasSeries("python-3.9_pandas-2.0.2");
+		unpicklePandasSeries("python-3.9_pandas-2.1.3");
 
 		unpicklePandasSeriesNA("python-3.9_pandas-1.4.1");
 		unpicklePandasSeriesNA("python-3.9_pandas-1.4.3");
 		unpicklePandasSeriesNA("python-3.9_pandas-1.5.1");
 		unpicklePandasSeriesNA("python-3.9_pandas-1.5.2");
 		unpicklePandasSeriesNA("python-3.9_pandas-2.0.2");
+		unpicklePandasSeriesNA("python-3.9_pandas-2.1.3");
 
 		unpicklePandasCategorical("python-3.9_pandas-1.4.1");
 		unpicklePandasCategorical("python-3.9_pandas-1.4.3");
 		unpicklePandasCategorical("python-3.9_pandas-1.5.1");
 		unpicklePandasCategorical("python-3.9_pandas-1.5.2");
 		unpicklePandasCategorical("python-3.9_pandas-2.0.2");
+		unpicklePandasCategorical("python-3.9_pandas-2.1.3");
 
 		unpicklePandasDataFrame("python-3.9_pandas-1.1.3");
 		unpicklePandasDataFrame("python-3.9_pandas-1.2.3");
@@ -186,12 +189,14 @@ public class DumpTest extends PickleUtilTest {
 		unpicklePandasDataFrame("python-3.9_pandas-1.5.1");
 		unpicklePandasDataFrame("python-3.9_pandas-1.5.2");
 		unpicklePandasDataFrame("python-3.9_pandas-2.0.2");
+		unpicklePandasDataFrame("python-3.9_pandas-2.1.3");
 
 		unpicklePandasDtypes("python-3.9_pandas-1.4.1");
 		unpicklePandasDtypes("python-3.9_pandas-1.4.3");
 		unpicklePandasDtypes("python-3.9_pandas-1.5.1");
 		unpicklePandasDtypes("python-3.9_pandas-1.5.2");
 		unpicklePandasDtypes("python-3.9_pandas-2.0.2");
+		unpicklePandasDtypes("python-3.9_pandas-2.1.3");
 	}
 
 	@Test
@@ -228,22 +233,27 @@ public class DumpTest extends PickleUtilTest {
 		unpicklePandasSeries("python-3.11_pandas-1.5.1");
 		unpicklePandasSeries("python-3.11_pandas-1.5.2");
 		unpicklePandasSeries("python-3.11_pandas-2.0.2");
+		unpicklePandasSeries("python-3.11_pandas-2.1.3");
 
 		unpicklePandasSeriesNA("python-3.11_pandas-1.5.1");
 		unpicklePandasSeriesNA("python-3.11_pandas-1.5.2");
 		unpicklePandasSeriesNA("python-3.11_pandas-2.0.2");
+		unpicklePandasSeriesNA("python-3.11_pandas-2.1.3");
 
 		unpicklePandasCategorical("python-3.11_pandas-1.5.1");
 		unpicklePandasCategorical("python-3.11_pandas-1.5.2");
 		unpicklePandasCategorical("python-3.11_pandas-2.0.2");
+		unpicklePandasCategorical("python-3.11_pandas-2.1.3");
 
 		unpicklePandasDataFrame("python-3.11_pandas-1.5.1");
 		unpicklePandasDataFrame("python-3.11_pandas-1.5.2");
 		unpicklePandasDataFrame("python-3.11_pandas-2.0.2");
+		unpicklePandasDataFrame("python-3.11_pandas-2.1.3");
 
 		unpicklePandasDtypes("python-3.11_pandas-1.5.1");
 		unpicklePandasDtypes("python-3.11_pandas-1.5.2");
 		unpicklePandasDtypes("python-3.11_pandas-2.0.2");
+		unpicklePandasDtypes("python-3.11_pandas-2.1.3");
 	}
 
 	private void unpickleBuiltinDtypes(String prefix) throws IOException {
