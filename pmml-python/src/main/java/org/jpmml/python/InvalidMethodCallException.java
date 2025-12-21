5b618e7ab7f31dbc0394fdccc1de94e68f175a1a
Villu Ruusmann <villu.ruusmann@gmail.com>
2025-12-21 21:47:30 +0200

    Added InvalidMethodCallException exception class

---
+++ pmml-python/src/main/java/org/jpmml/python/InvalidMethodCallException.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2025 Villu Ruusmann
+ *
+ * This file is part of JPMML-Python
+ *
+ * JPMML-Python is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JPMML-Python is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with JPMML-Python.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jpmml.python;
+
+import java.util.List;
+
+import org.jpmml.converter.OperationException;
+
+public class InvalidMethodCallException extends OperationException {
+
+	public InvalidMethodCallException(String dottedName, List<String> parameters, List<?> arguments){
+		super(formatMessage(dottedName, parameters, arguments));
+	}
+
+	static
+	private String formatMessage(String dottedName, List<String> parameters, List<?> arguments){
+		String nameAndSignature = dottedName + "(" + String.join(", ", parameters) + ")";
+
+		return "Method \'" + nameAndSignature + "\' expects " + parameters.size() + " argument(s), got " + arguments.size() + " argument(s)";
+	}
+}
